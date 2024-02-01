7ced8643606f4cccb41880a991233df0ec87eeca
edinhokrs <edinhokrause@hotmail.com>
2024-01-31 22:36:18 -0300

    update tela login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  usuario: string = '';
+  senha: string = '';
+  mensagemErro: string = '';
 
+  fazerLogin() {
+    // Adicione sua lógica de autenticação aqui
+    if (this.usuario === 'DWU' && this.senha === 'DWU2023') {
+      // Lógica de sucesso de login (pode redirecionar para outra página, por exemplo)
+      console.log('Login bem-sucedido!');
+    } else {
+      this.mensagemErro = 'Usuário ou senha incorretos';
+    }
+  }
 }
